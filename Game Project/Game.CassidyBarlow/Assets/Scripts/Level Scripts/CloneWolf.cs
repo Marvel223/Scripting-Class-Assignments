a73ecbce72b1fb015d3097051a1051bf6aa595f9
Marvel223 <cassidy_barlow@yahoo.com>
2016-11-03 12:56:27 -0600

    Timer Menu working, needs to add wolf, make stars bounce

---
+++ Game Project/Game.CassidyBarlow/Assets/Scripts/Level Scripts/CloneWolf.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloneWolf : MonoBehaviour 
+{
+	public Transform[] wolfSpawnPoints;
+	public Transform wolf;
+	public float wolfSpawnFrequeny;
+	public bool canSpawnWolf;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
