c2ce6012f388d1bf0e436ca978abb997d216989b
Unknown <cassidy_barlow@yahoo.com>
2016-11-14 14:00:16 -0700

    Interfaces Lesson 11/14/16

---
+++ InClass Unity/10.17/InClassUnity/Assets/InterFaces/AddPoints.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class AddPoints : MonoBehaviour, IAddPoints{
+    public int points
+    {
+        get
+        {
+            throw new NotImplementedException();
+        }
+
+        set
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public void OnTriggerEnter()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ InClass Unity/10.17/InClassUnity/Assets/InterFaces/IPowerUp.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IPowerUp {
+
+    int power
+    {
+        get;
+    }
+
+    void Start();
+
+    void OnTriggerEnter();
+
+	
+}
