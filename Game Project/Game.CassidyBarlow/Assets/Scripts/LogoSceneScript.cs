684e9058e969474009a92b61c92defff5e4879e9
Unknown <cassidy_barlow@yahoo.com>
2016-12-01 12:38:59 -0700

    Made Changes to menu's, UVU logo added

---
+++ Game Project/Game.CassidyBarlow/Assets/Scripts/LogoSceneScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LogoSceneScript : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+        StartCoroutine(GoToStartMenu());
+    }
+	
+    IEnumerator GoToStartMenu()
+    {
+        yield return new WaitForSeconds(7);
+        int scene = SceneManager.GetSceneByName("MenuScene").buildIndex;
+        SceneManager.LoadScene(0, LoadSceneMode.Single);
+    }
+
+}
