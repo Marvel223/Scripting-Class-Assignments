67134864d50ca96491a365919da62b689e7b988b
Cassidy Barlow <cassidy_barlow@yahoo.com>
2016-12-07 21:03:02 -0700

    Create my own delegate with Taylors help

---
+++ InClass Unity/10.17/InClassUnity/Assets/Delegates/NewDelegates.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class NewDelegates : MonoBehaviour 
+{
+	public static Action<float> SpeedOnStart;
+	public static Action<float> SpeedOnTrigger;
+
+	void Start()
+	{
+		
+		StartCoroutine (MoveDelay ());
+	}
+
+	IEnumerator MoveDelay()
+	{
+		SpeedOnStart (0.01f);
+		yield return null;
+		Start ();
+}
+}
---
+++ InClass Unity/10.17/InClassUnity/Assets/Delegates/NewHandler.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewHandler : MonoBehaviour {
+
+	public float speed;
+	private Vector3 tempPos;
+	private CharacterController myCC;
+
+
+	// Use this for initialization
+	void Start () {
+		NewDelegates.SpeedOnStart += SpeedOnStartHandler;
+		myCC = GetComponent<CharacterController> ();
+
+
+	}
+
+	void SpeedOnStartHandler(float T)
+	{
+		speed = T;
+		tempPos.x = speed;
+		myCC.Move(tempPos);
+	}
+
+	void Update()
+	{
+		
+
+			
+	}
+
+
+	void OnTriggerEnter()
+	{
+		
+	}
+
+
+}
